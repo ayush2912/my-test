214c82b655e715118c9de997cb04052d6313c07a
Vishal Mishra <vishal@Vishals-MacBook-Pro.local>
2023-02-19 13:41:23 +0530

    Mongoose added | structure changes

---
+++ src/schema/order.ts
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+//Create orderschema interface and associate type here
+const OrderSchema = new Schema({
+    partnerId: Schema.Types.ObjectId,
+    lineItems: [{ type: Schema.Types.ObjectId, ref: 'LineItem' }],
+    quotations: [{ type: Schema.Types.ObjectId, ref: 'Quotation' }],
+    execution: Schema.Types.ObjectId,
+    totalPrice: { type: Number, required: true },
+    status: { type: Number, required: true }
+});
