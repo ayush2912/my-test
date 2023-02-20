6f50cb0ff7da51cfcc243c01a7c3f95b0b8d765e
Grey Ang <conancat@gmail.com>
2023-02-20 12:36:23 +0800

    added route test examples

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,16 @@
 import { Router, Request, Response } from "express";
 
-export const index = (req: Request, res: Response) =>
+export const routeHandler = (req: Request, res: Response) =>
   res.send(
     '<div class="tenor-gif-embed" data-postid="23513555" data-share-method="host" data-aspect-ratio="1.77778" data-width="100%"><a href="https://tenor.com/view/sheldon-cooper-bazinga-tbbt-gif-23513555">Sheldon Cooper GIF</a>from <a href="https://tenor.com/search/sheldon-gifs">Sheldon GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>'
   );
 
+export const asyncRouteHandler = async (req: Request, res: Response) =>
+  res.json({
+    data: "Hello world!",
+  });
+
 export default function routes(router: Router) {
-  router.get("/", index);
+  router.get("/", routeHandler);
+  router.get("/async", asyncRouteHandler);
 }
