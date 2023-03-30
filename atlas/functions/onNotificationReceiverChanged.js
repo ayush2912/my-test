505cb9abc451c5227843d4f351d187f1cc3fb22a
Grey Ang <conancat@gmail.com>
2023-03-30 19:05:09 +0800

    feat: added environment variables for application

--- atlas/functions/onNotificationReceiverChanged.js
+++ atlas/functions/onNotificationReceiverChanged.js
@@ -1,19 +1,19 @@
 exports = async function (changeEvent) {
   const receiverId = changeEvent.fullDocument.receiverId;
-  
+
   const requestOptions = {
-    url: "https://offsetmax-notification-transport-japrkvsz2a-em.a.run.app/notify",
-    body: JSON.stringify({ userId: receiverId }), 
+    url: context.environment.values.NOTIFICATION_TRANSPORT_API_URL,
+    body: JSON.stringify({ userId: receiverId }),
     headers: {
-      "Content-Type": [ "application/json" ]
-    }
-  }
+      "Content-Type": ["application/json"],
+    },
+  };
 
   const response = await context.http.post(requestOptions);
-  
-  console.log(JSON.stringify(requestOptions))
-  console.log(response.body.text())
-  console.log(JSON.stringify(changeEvent))
+
+  console.log(JSON.stringify(requestOptions));
+  console.log(response.body.text());
+  console.log(JSON.stringify(changeEvent));
 
   return EJSON.parse(response.body.text());
 };
