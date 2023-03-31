9f5326bade549b84cb54871cdd49c0efb89d0340
Grey Ang <conancat@gmail.com>
2023-03-31 16:10:18 +0800

    feat: updated atlas config to support different environments

--- atlas/functions/onNotificationReceiverChanged.js
+++ atlas/functions/onNotificationReceiverChanged.js
@@ -1,8 +1,22 @@
+const notificationTransportUrls = {
+  offsetmaxsandbox:
+    "https://offsetmax-notification-transport-dev-japrkvsz2a-em.a.run.app/notify",
+  "offsetmax-demo":
+    "https://offsetmax-notification-transport-demo-japrkvsz2a-em.a.run.app/notify",
+  "offsetmax-prod":
+    "https://offsetmax-notification-transport-prod-japrkvsz2a-em.a.run.app/notify",
+  "offsetmax-staging":
+    "https://offsetmax-notification-transport-staging-japrkvsz2a-em.a.run.app/notify",
+  "offsetmax-prod-mirror":
+    "https://offsetmax-notification-transport-prod-mirror-japrkvsz2a-em.a.run.app/notify",
+};
+
 exports = async function (changeEvent) {
   const receiverId = changeEvent.fullDocument.receiverId;
+  const url = notificationTransportUrls[changeEvent.ns.db];
 
   const requestOptions = {
-    url: context.environment.values.NOTIFICATION_TRANSPORT_API_URL,
+    url: url,
     body: JSON.stringify({ userId: receiverId }),
     headers: {
       "Content-Type": ["application/json"],
