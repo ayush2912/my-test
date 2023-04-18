504b9598ef55db7d83ff26dbb3a132d66e40b15d
Kuan Chien <kcyeo29@gmail.com>
2023-04-18 14:47:25 +0800

    setup path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,14 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.js',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      components: `${path.resolve(__dirname, './src/components/')}`,
+      public: `${path.resolve(__dirname, './public/')}`,
+      pages: path.resolve(__dirname, './src/pages'),
+      types: `${path.resolve(__dirname, './src/@types')}`,
+    },
   },
 });
