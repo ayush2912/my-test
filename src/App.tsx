af4198baf1f64e1294c9bccbf18dc91dd9de5b10
Kuan Chien <kcyeo29@gmail.com>
2023-04-19 14:18:05 +0800

    setup theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import './App.css';
+import styled, { ThemeProvider } from "styled-components";
+
+import "./App.css";
+import { theme } from "./styles/theme";
+
+const Button = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  font-size: ${(props) => props.theme.typography.button.size}px;
+`;
 
 function App() {
-  return <div className="App">this is a CCD REACT boilerplate project</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Button>hello</Button>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,75 @@
+export const theme = {
+  colors: {
+    primary: {
+      50: "#EFF4FE",
+      100: "#D8E4FC",
+      200: "#B1C8F9",
+      300: "#8AADF7",
+      400: "#6391F4",
+      500: "#3C76F1",
+      600: "#2D59B5",
+      700: "#1E3B79",
+      800: "#0F1E3C",
+      900: "#060C18",
+    },
+    neutral: {
+      0: "#FFFFFF",
+      50: "##F1F2F4",
+      100: "#E1E4E8",
+      200: "#C4C9D1",
+      300: "#A6ADBA",
+      400: "#8992A3",
+      500: "#6B778C",
+      600: "#505969",
+      700: "#363C46",
+      800: "#1B1E23",
+      900: "#0B0C0E",
+    },
+    background: {
+      default: "#F1F2F4",
+      card: "#FFFFFF",
+    },
+    text: {
+      default: "#363C46",
+      subdued: "#8992A3",
+      disabled: "#C4C9D1",
+      white: "#FFFFFF",
+    },
+    semantic: {
+      error: {
+        default: "#FF647C",
+        subdued: "#FBE4E8",
+      },
+      success: {
+        default: "#00A676",
+        subdued: "#E2F8F2",
+      },
+      warning: {
+        default: "#E0A008",
+        subdued: "#FFF8E7",
+      },
+      information: {
+        default: "#0084F4",
+        subdued: "#D5E9FA",
+      },
+    },
+  },
+  spacing: {
+    padding: {
+      small: 10,
+      medium: 20,
+      large: 30,
+    },
+    borderRadius: {
+      small: 5,
+      default: 10,
+    },
+  },
+  typography: {
+    button: {
+      weight: "600",
+      size: 16,
+      lineHeight: 24,
+    },
+  },
+};
