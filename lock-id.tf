65759c0700406f98ad1e06c1a57494411ad1d81b
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2023-04-21 22:33:49 +0530

    Update lock-id.tf

--- lock-id.tf
+++ lock-id.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-offsetmax"
-    key            = "-transport-notification/remote/s3/ccd/terraform.tfstate"
+    key            = "-transport-notification/remote/s3/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "dynamodb-state-locking-terraform"
   }
