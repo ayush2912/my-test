78caddeb92f35ecb4dc57b0c552f4e6989618bda
pranjalccd <pranjal.gupta@climateconnect.digital>
2023-04-22 12:16:08 +0530

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,49 @@
-import { Request, Response, NextFunction } from 'express';
-import express from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 
 import routes from './routes/postRoute';
+
 import {
     constructResponse,
     constructErrorResponse,
     safelySetHeaders,
 } from './ServiceHandler';
+
 import formatSuccessResponse from './utility/ResponseEnvelope';
 
-const app = express();
+const app: Express = express();
 const PORT = 8080;
 
 app.use(express.json());
 app.use(helmet());
+
+// Handle CORS error
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+const whitelist = ['http://localhost:3000', 'https://dev-dashboard.offsetmax.digital/', 'https://staging-dashboard.offsetmax.digital/'];
+let corsOptions = {
+    origin: ((origin: any, callback: any) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    })
+};
+
+app.use(cors(corsOptions));
 
 declare global {
     namespace Express {
