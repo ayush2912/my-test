a5c27968d804334dbc62affd2f880f394a78161b
pranjalccd <pranjal.gupta@climateconnect.digital>
2023-04-23 22:01:59 +0530

    added validation mw at route layer

--- src/routes/postRoute.ts
+++ src/routes/postRoute.ts
@@ -1,36 +1,48 @@
 import { Request, Response, Router } from 'express';
 import { getPostData, createPostData } from '../services/postService';
+import {
+    validatePostIdParamsSchema,
+    validateCreatePostSchema,
+} from '../middlewares/validation';
 
 export default function routes(router: Router) {
-    router.get('/post/:id', async (req: Request, res: Response) => {
-        try {
-            console.info('----- /post/:id ----');
-            const results = await getPostData(req.params.id);
+    router.get(
+        '/post/:id',
+        validatePostIdParamsSchema,
+        async (req: Request, res: Response) => {
+            try {
+                console.info('----- /post/:id ----');
+                const results = await getPostData(req.params.id);
 
-            res.sendSuccess({
-                data: results.title,
-                msg: 'get posts successfully',
-            });
-        } catch (error) {
-            res.sendError(error);
+                res.sendSuccess({
+                    data: results,
+                    msg: 'get posts successfully',
+                });
+            } catch (error) {
+                res.sendError(error);
+            }
         }
-    });
+    );
 
-    router.post('/createPost', async (req: Request, res: Response) => {
-        try {
-            console.info('--- /createPost ----');
-            const newData = await createPostData(
-                req.body.title,
-                req.body.content
-            );
+    router.post(
+        '/post',
+        validateCreatePostSchema,
+        async (req: Request, res: Response) => {
+            try {
+                console.info('--- /createPost ----');
+                const newData = await createPostData(
+                    req.body.title,
+                    req.body.content
+                );
 
-            res.sendSuccess({
-                status_code: 201,
-                data: newData,
-                msg: 'checking successfully',
-            });
-        } catch (error) {
-            res.sendError(error);
+                res.sendSuccess({
+                    status_code: 201,
+                    data: newData,
+                    msg: 'Post created Successfully',
+                });
+            } catch (error) {
+                res.sendError(error);
+            }
         }
-    });
+    );
 }
