0463dff155e60ed9d14af607487d5e11a9c2586d
Kuan Chien <kcyeo29@gmail.com>
2023-04-24 16:23:23 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,10 @@ import styled, { ThemeProvider } from "styled-components";
 import "./App.css";
 import { theme } from "./styles/theme";
 
-const Button = styled.button`
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border-radius: 3px;
-  font-size: ${(props) => props.theme.typography.button.size}px;
-`;
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Button>hello</Button>
-      </div>
+      <div className="App">This is CCD new project</div>
     </ThemeProvider>
   );
 }
