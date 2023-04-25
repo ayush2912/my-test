c7783486fae1a7a0718ce220f38fc0fc0930c4c6
pranjalccd <pranjal.gupta@climateconnect.digital>
2023-04-25 14:00:12 +0530

    deleted unwanted files

--- src/utility/constants/PostConstants.ts
+++
@@ -1,9 +0,0 @@
-const PostConstants = {
-    INVALID_POST_ID: 'Invalid PostId',
-    TITLE_REQUIRED: 'title is required',
-    INVALID_TITLE: 'title must be a string',
-    CONTENT_REQUIRED: 'content is required',
-    INVALID_CONTENT: 'content must be a string',
-};
-
-export default PostConstants;
