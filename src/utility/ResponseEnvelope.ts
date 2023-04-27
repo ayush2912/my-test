39853b6667a072550a621405fa1b0937b575f50d
pranjalccd <pranjal.gupta@climateconnect.digital>
2023-04-27 19:34:37 +0530

    updated cutom_code to camnel case

--- src/utility/ResponseEnvelope.ts
+++ src/utility/ResponseEnvelope.ts
@@ -3,21 +3,21 @@ import SuccessResponse from '../interfaces/successResponse.interface';
 
 /**
  * @description This method will return a envelope for success response
- * @param { Object } { data, status_code, msg, custom_code }
- * @returns { Object } { status, status_code, message, data, custom_code }
+ * @param { Object } { data, status_code, msg, customCode }
+ * @returns { Object } { status, status_code, message, data, customCode }
  */
 function formatSuccessResponse({
     data = {},
-    status_code,
+    statusCode,
     msg = constants.DEFAULT_SUCCESS_MESSAGE,
-    custom_code = 'DEFAULT_SUCCESS_MESSAGE',
+    customCode = 'DEFAULT_SUCCESS_MESSAGE',
 }: SuccessResponse) {
     return {
         status: constants.SUCCESS,
-        status_code: status_code,
+        statusCode: statusCode,
         message: msg,
         data: data,
-        custom_code: custom_code,
+        customCode: customCode,
     };
 }
 
