5f8aa58687b97b42c8aa739a4960c582a002a66b
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2023-04-27 12:12:39 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable SOCKETIO_MONGO_ADAPTER_URI {
+  default = ""
+}
+
+variable SOCKETIO_MONGO_ADAPTER_DB {
+  default = ""
+}
+
+variable SOCKETIO_MONGO_ADAPTER_COLLECTION {
+  default = ""
+}
+
+variable GQL_API {
+  default = ""
+}
