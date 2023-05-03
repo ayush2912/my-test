bcbffdf739a466de12a45f5f520f1243f8d08f1f
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2023-05-03 15:27:32 +0530

    fix: task list story update

--- src/modules/projects/components/TaskList.stories.tsx
+++ src/modules/projects/components/TaskList.stories.tsx
@@ -36,10 +36,10 @@ export const Primary: Story = {
       />
       <TaskList
         isOverdue={false}
-        type="Task 4"
+        type="Task 7"
         startDate={new Date("2023-01-10T06:20:44.069Z")}
         dueDate={new Date("2030-01-05T08:52:29.962Z")}
-        state="IN_PROGRESS"
+        state="DISCONTINUED"
       />
       <TaskList
         isOverdue={true}
