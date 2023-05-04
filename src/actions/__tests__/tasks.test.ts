d7397f7f07c00592683ff9bcc5ed794fba5a4e09
rachanachikkacc <109352793+rachanachikkacc@users.noreply.github.com>
2023-05-04 13:04:47 +0530

    added return type for createMany tasks

--- src/actions/__tests__/tasks.test.ts
+++ src/actions/__tests__/tasks.test.ts
@@ -1,11 +1,7 @@
 import { faker } from '@faker-js/faker';
 import prisma from '../prisma';
-import {
-    createTasks, updateTask, deleteTask
-} from '../tasks';
-import {
-    deleteProject
-} from '../projects';
+import { createTasks, updateTask, deleteTask } from '../tasks';
+import { deleteProject } from '../projects';
 const project = {
     id: faker.database.mongodbObjectId(),
     name: faker.company.name(),
@@ -15,93 +11,123 @@ const project = {
 const engagement = {
     id: faker.database.mongodbObjectId(),
     projectId: project.id,
-    type: "Getting a Project Listed",
+    type: 'Getting a Project Listed',
     startDate: faker.date.recent(),
-    dueDate: faker.date.future()
-}
+    dueDate: faker.date.future(),
+};
 
-beforeAll(async () => Promise.all([
-    prisma.project.create({ data: project }),
-    prisma.engagement.create({ data: engagement })
-]));
+beforeAll(async () =>
+    Promise.all([
+        prisma.project.create({ data: project }),
+        prisma.engagement.create({ data: engagement }),
+    ])
+);
 
-afterAll((async () => deleteProject(project.id)));
+afterAll(async () => deleteProject(project.id));
 
 describe('createTasks()', () => {
     test('it should create tasks successfully', async () => {
         const data = [
             {
                 engagementId: engagement.id,
-                type: "PPD",
+                type: 'PPD',
                 startDate: faker.date.recent(),
                 dueDate: faker.date.future(),
-                stateHistory: {
-                    state: "NOT_STARTED",
-                    stateUpdatedAt: faker.date.recent()
-                }
+                stateHistory: [
+                    {
+                        state: 'NOT_STARTED',
+                        stateUpdatedAt: faker.date.recent(),
+                    },
+                ],
             },
             {
                 engagementId: engagement.id,
-                type: "submitted the document",
+                type: 'submitted the document',
                 startDate: faker.date.recent(),
                 dueDate: faker.date.future(),
-                stateHistory: {
-                    state: "NOT_STARTED",
-                    stateUpdatedAt: faker.date.recent()
-                }
+                stateHistory: [
+                    {
+                        state: 'NOT_STARTED',
+                        stateUpdatedAt: faker.date.recent(),
+                    },
+                ],
+            },
+        ];
+        const createdTasks = await createTasks(data);
+
+        expect(createdTasks.length).toEqual(data.length);
+        createdTasks.sort((a: any, b: any) => a.type - b.type);
+        data.sort((a: any, b: any) => a.type - b.type);
+
+        for (let i = 0; i < createdTasks.length; i++) {
+            const given = data[i];
+            const created = createdTasks[i];
+            expect(given.type).toEqual(created.type);
+            expect(given.startDate).toEqual(created.startDate);
+            expect(given.dueDate).toEqual(created.dueDate);
+            expect(given.engagementId).toEqual(created.engagementId);
+            expect(given.stateHistory.length).toEqual(
+                created.stateHistory?.length
+            );
+            for (let i = 0; i < given.stateHistory.length; i++) {
+                expect(given.stateHistory[i].state).toEqual(
+                    created.stateHistory?.[i]?.state
+                );
+                expect(given.stateHistory[i].stateUpdatedAt).toEqual(
+                    created.stateHistory?.[i]?.stateUpdatedAt
+                );
             }
-        ]
-        const result = await createTasks(data);
-
-        expect(result?.count).toEqual(2);
-
+        }
+        const taskIds: string[] = [];
+        for (let i = 0; i < createdTasks.length; i++) {
+            taskIds.push(createdTasks[i].id!);
+        }
         prisma.task.deleteMany({
             where: {
-                engagementId: {
-                    equals: engagement.id
-                }
-            }
-        })
-    })
-})
+                id: {
+                    in: taskIds,
+                },
+            },
+        });
+    });
+});
 
 describe('updateTask()', () => {
     test('it should update task successfully', async () => {
-
         const task = await prisma.task.create({
             data: {
                 id: faker.database.mongodbObjectId(),
                 engagement: {
                     connect: {
-                        id: engagement.id
-                    }
+                        id: engagement.id,
+                    },
                 },
-                type: "Submit the Document",
+                type: 'Submit the Document',
                 startDate: faker.date.recent(),
-                dueDate: faker.date.future()
-            }
-        })
-        
+                dueDate: faker.date.future(),
+            },
+        });
+
         const data = {
-                type: "PPD",
-                startDate: faker.date.recent(),
-                dueDate: faker.date.future()
-            }
+            type: 'PPD',
+            startDate: faker.date.recent(),
+            dueDate: faker.date.future(),
+        };
         const result = await updateTask(task.id, data);
 
         expect(result?.type).toEqual(data.type);
         expect(result?.startDate).toEqual(data.startDate);
         expect(result?.dueDate).toEqual(data.dueDate);
-        
+
         prisma.task.deleteMany({
             where: {
                 engagementId: {
-                    equals: engagement.id
-                }
-            }
-        })
-    })
-})
+                    equals: engagement.id,
+                },
+            },
+        });
+    });
+});
 
 describe('deleteTask()', () => {
     test('it should delete task successfully', async () => {
@@ -110,23 +136,22 @@ describe('deleteTask()', () => {
                 id: faker.database.mongodbObjectId(),
                 engagement: {
                     connect: {
-                        id: engagement.id
-                    }
+                        id: engagement.id,
+                    },
                 },
-                type: "Submit the Document",
+                type: 'Submit the Document',
                 startDate: faker.date.recent(),
-                dueDate: faker.date.future()
-            }
-        })
-
+                dueDate: faker.date.future(),
+            },
+        });
 
         if (!task.id) {
-            throw new Error('Engagement not created');
+            throw new Error('Task not created');
         }
 
         const result = await deleteTask(task.id);
 
         expect(result.type).toEqual(task.type);
         expect(result.id).toEqual(task.id);
-    })
-})
+    });
+});
--- src/actions/tasks.ts
+++ src/actions/tasks.ts
@@ -1,10 +1,13 @@
-import { Prisma, PrismaClient, Task } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { ObjectId } from 'mongodb';
 
 const prisma = new PrismaClient();
 
 const TaskSchema: Prisma.TaskSelect = {
     id: true,
     engagementId: true,
+    createdAt: true,
+    updatedAt: true,
     type: true,
     startDate: true,
     dueDate: true,
@@ -15,30 +18,61 @@ const TaskSchema: Prisma.TaskSelect = {
             state: true,
             stateUpdatedAt: true,
         },
-    }
+    },
 };
 
-const createTasks = async (data: any) => 
-  prisma.task.createMany({
-        data: data
+const createTasks = async (
+    data: {
+        id?: string;
+        type: string;
+        startDate: Date;
+        dueDate: Date;
+        engagementId: string;
+        stateHistory?: {
+            state: string;
+            stateUpdatedAt: Date;
+        }[];
+    }[]
+) => {
+    const mongodbIds = [];
+    for (let i = 0; i < data.length; i++) {
+        mongodbIds.push(new ObjectId().toHexString());
+        data[i].id = mongodbIds[i];
+    }
+    await prisma.task.createMany({
+        data: data,
     });
+    return await prisma.task.findMany({
+        where: {
+            id: {
+                in: mongodbIds,
+            },
+        },
+        select: TaskSchema,
+    });
+};
 
-
-const updateTask = async (taskId: string,data: any) =>
+const updateTask = async (
+    taskId: string,
+    data: {
+        startDate?: Date;
+        dueDate?: Date;
+        engagementId?: string;
+        completedDate?: string;
+    }
+) =>
     prisma.task.update({
         data: data,
         where: {
-            id: taskId
-        }
-    })
+            id: taskId,
+        },
+    });
 
 const deleteTask = async (taskId: string) =>
     prisma.task.delete({
         where: {
-            id: taskId
-        }
-    })
+            id: taskId,
+        },
+    });
 
-
-
-export {createTasks, updateTask, deleteTask}
+export { createTasks, updateTask, deleteTask };
