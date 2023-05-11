18c4c50fc5ede384db6259d38bad2739a287e288
pranjalccd <pranjal.gupta@climateconnect.digital>
2023-05-12 03:03:06 +0530

    feat: updtae and delete project with strapi along with routes

--- src/webhooks/Project/projectService.ts
+++ src/webhooks/Project/projectService.ts
@@ -1,23 +1,44 @@
-import { createProject } from '../../actions/projects'
-import { mappedProjectData } from './projectUtility';
+import {
+    getProject,
+    createProject,
+    deleteProject,
+    getProjectsByStrapiId,
+    updateProjectData,
+} from '../../actions/projects';
+
+import { deleteEngagementsByStrapiIds } from '../../actions/enagagements';
+import { deleteTasksByStrapiIds } from '../../actions/tasks';
+
+import {
+    getProjectOwners,
+    mappedProjectData,
+    mappedProjectWithStrapi,
+    deletedStrapiIds
+} from './projectUtility';
+
+import { compareAndUpdate } from '../../utility/utils';
+import Errors from '../../errors';
 
 /**
  * This method used to create project form strapi-webhook.
- * @param {Object} data payload of the project
- * @returns {Object}  data of the project details
+ * @param {string} entryId strapi id of the project
+ * @param {object} data payload of the project
+ * @returns {object}  data of the project details
  */
-async function createProjectStrapi(data: any) {
+async function createProjectStrapi(entryId: number, data: any) {
     try {
         console.info(
             '----- In Webhook projectService of createProjectStrapi method ------'
         );
 
-        const mappedData = mappedProjectData(data);
+        const projectOwners = await getProjectOwners(entryId);
+
+        const combinedData = { ...data, ...projectOwners };
+        const mappedData = mappedProjectData(combinedData);
 
         const projectDetails = await createProject(mappedData);
 
         return projectDetails;
-
     } catch (error) {
         console.error(
             '***** Error in Webhook projectService of createProjectStrapi method *****',
@@ -27,4 +48,87 @@ async function createProjectStrapi(data: any) {
     }
 }
 
-export { createProjectStrapi };
+/**
+ * This method used to update project form strapi-webhook.
+ * @param {string} entryId strapi id of the project
+ * @param {object} data payload of the project
+ * @returns {object}  data of the project details
+ */
+async function updateProjectStrapi(entryId: number, data: any) {
+    try {
+        console.info(
+            '----- In Webhook projectService of updateProjectStrapi method ------'
+        );
+
+        const project = await getProjectsByStrapiId(entryId.toString());
+
+        if (!project.length) {
+            throw new Errors.BadRequest('Project do not exist in system');
+        }
+
+        const projectId = project[0].id;
+        const getDetails = await getProject(projectId);
+
+        const projectOwners = await getProjectOwners(entryId);
+        const combinedData = { ...data, ...projectOwners };
+
+        const mappedData = mappedProjectData(combinedData);
+        const mappedDatawithStarpi = mappedProjectWithStrapi(getDetails);
+
+        const updatedData = compareAndUpdate(mappedDatawithStarpi, mappedData)
+
+        const deletedIds = await deletedStrapiIds(getDetails, mappedData);
+
+        if (deletedIds.missingTaskIds) {
+            await deleteTasksByStrapiIds(deletedIds.missingTaskIds);
+        }
+
+        if (deletedIds.missingEngagementIds > 0) {
+            await deleteEngagementsByStrapiIds(deletedIds.missingEngagementIds);
+        }
+
+        const updatedDetails = await updateProjectData(projectId, updatedData);
+
+        return updatedDetails;
+
+    } catch (error) {
+        console.error(
+            '***** Error in Webhook projectService of updateProjectStrapi method *****',
+            error
+        );
+        throw error;
+    }
+}
+
+/**
+ * This method used to delete project.
+ * @param {number} entryId id of the project
+ * @param {object} data payload of the project
+ * @returns {object}  data of the project details
+ */
+async function deleteProjectStrapi(strapiId: number) {
+    try {
+        console.info(
+            '----- In Webhook projectService of deleteProjectStrapi method ------'
+        );
+
+        const project = await getProjectsByStrapiId(strapiId.toString());
+
+        if (!project.length) {
+            throw new Errors.BadRequest('Project do not exist in system');
+        }
+
+        const projectId = project[0].id;
+        const deletedProjectDetails = await deleteProject(projectId);
+
+        return deletedProjectDetails;
+    } catch (error) {
+        console.error(
+            '***** Error in Webhook projectService of deleteProjectStrapi method *****',
+            error
+        );
+        throw error;
+    }
+}
+
+export { createProjectStrapi, updateProjectStrapi, deleteProjectStrapi };
--- src/webhooks/webhook.route.ts
+++ src/webhooks/webhook.route.ts
@@ -1,60 +1,95 @@
-
 import { Request, Response, Router } from 'express';
-import { createProjectStrapi } from './Project/projectService';
+import {
+    createProjectStrapi,
+    updateProjectStrapi,
+    deleteProjectStrapi,
+} from './Project/projectService';
+
+import { events, EventTypes } from '../services/events';
 
 export default function webhookRoutes(router: Router) {
-    router.post(
-        '/strapi-webhook',
-        async (req: Request, res: Response) => {
-            try {
-                console.info('----- /strapi-webhook ----');
+    router.post('/strapi-webhook', async (req: Request, res: Response) => {
+        try {
+            console.info('----- /strapi-webhook ----');
 
-               console.log(JSON.stringify(req.body, null, 2));
+            console.log(JSON.stringify(req.body, null, 2));
 
-                const payload = req.body;
-                const event = payload.event;
-                const model = payload.model;
-                //const entryId = payload.entry.id;
+            const payload = req.body;
+            const event = payload.event;
+            const model = payload.model;
+            const entryId = payload.entry.id;
 
-                switch (event) {
-                    case 'entry.create':
-                        switch (model) {
-                            case 'project':
-                                // Handle project create event
-                                const results = await createProjectStrapi(payload.entry);
-                                console.log(results);
-                                //here call slack notification messsanger
+            switch (event) {
+                case 'entry.create':
+                    switch (model) {
+                        case 'project':
 
-                                break;
-                        }
-                        break;
-                    case 'entry.update':
-                        switch (model) {
-                            case 'project':
-                                // Handle project update event
-                                break;
-                        }
-                        break;
-                    case 'entry.delete':
-                        switch (model) {
-                            case 'project':
-                                // Handle project delete event
-                                break;
-                        }
-                        break;
-                    default:
-                        // Handle unknown event
-                        break;
-                }
+                            events.emit(
+                                EventTypes.CREATING_STRAPI_PROJECT,
+                                payload.entry.ProjectName
+                            );
+                            try {
+                                await createProjectStrapi(
+                                    entryId,
+                                    payload.entry
+                                );
+                            } catch (error) {
+                                events.emit(EventTypes.STRAPI_PROJECT_VALIDATION_FAILED,
+                                    payload.entry.ProjectName,
+                                    error);
+                            }
 
-                // res.sendSuccess({
-                //     msg: ProjectConstants.PROJECT_DETAILS_RETRIEVED,
-                //     data: results,
-                //     customCode: 'PROJECT_DETAILS_RETRIEVED_SUCCESSFULLY',
-                // });
-            } catch (error) {
-                res.sendError(error);
+                            break;
+                    }
+                    break;
+                case 'entry.update':
+                    switch (model) {
+                        case 'project':
+
+                            events.emit(
+                                EventTypes.UPDATING_STRAPI_PROJECT,
+                                payload.entry.ProjectName
+                            );
+                            try {
+                                await updateProjectStrapi(entryId, payload.entry);
+                            }
+                            catch (error) {
+                                events.emit(EventTypes.STRAPI_PROJECT_VALIDATION_FAILED,
+                                    payload.entry.ProjectName,
+                                    error);
+                            }
+
+                            break;
+                    }
+                    break;
+                case 'entry.delete':
+                    switch (model) {
+                        case 'project':
+
+                            events.emit(
+                                EventTypes.DELETING_STRAPI_PROJECT,
+                                payload.entry.ProjectName
+                            );
+                            try {
+                                await deleteProjectStrapi(
+                                    entryId
+                                );
+
+                            } catch (error) {
+                                events.emit(EventTypes.STRAPI_PROJECT_VALIDATION_FAILED,
+                                    payload.entry.ProjectName,
+                                    error);
+                            }
+
+                            break;
+                    }
+                    break;
+                default:
+                    // Handle unknown event
+                    break;
             }
+        } catch (error) {
+            res.sendError(error);
         }
-    );
+    });
 }
