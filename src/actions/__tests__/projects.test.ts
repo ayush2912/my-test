8276bcba2cedd5fb300f2c465a5ca634e0de6f9e
Sharad Mishra <sharad.mishra@climate-connect.com>
2023-05-12 10:59:26 +0530

    organizationId fix

--- src/actions/__tests__/projects.test.ts
+++ src/actions/__tests__/projects.test.ts
@@ -450,10 +450,10 @@ describe('getProjects()', () => {
     it('returns null if the organization does not exist', async () => {
         const organizationId = '5116591277702d2113142ebc';
         const result = await getProjects({
-            organizationIds: [ organizationId ],
+            organizationIds: [organizationId],
             take: 10,
             skip: 0,
-            tab: 'ACTIVE'
+            tab: 'ACTIVE',
         });
         expect(result).toEqual([]);
     });
