bda59b0a0bca761ccacb62f7a989940eebc4aa4d
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2023-05-30 15:14:03 +0530

    feat: new story added for select and comp update

--- src/components/Select.stories.tsx
+++ src/components/Select.stories.tsx
@@ -10,10 +10,63 @@ const meta: Meta<typeof Select> = {
 export default meta;
 type Story = StoryObj<typeof Select>;
 
+const options = [
+  { value: "value 1", displayValue: "Display Value" },
+  {
+    value: "value 2",
+    displayValue: "Value with sub value",
+    subValue: "(sub value)",
+  },
+  {
+    value: "long value",
+    displayValue:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  },
+  {
+    value: "value 3",
+    displayValue: "Value with sub value",
+    subValue: "(sub value)",
+  },
+  {
+    value: "value 4",
+    displayValue: "Value with sub value",
+    subValue: "(sub value)",
+  },
+  {
+    value: "value 5",
+    displayValue: "Value with sub value",
+    subValue: "(sub value)",
+  },
+  {
+    value: "value 6",
+    displayValue: "Value with sub value",
+    subValue: "(sub value)",
+  },
+  {
+    value: "value 7",
+    displayValue: "Value with sub value",
+    subValue: "(sub value)",
+  },
+  {
+    value: "value 8",
+    displayValue: "Value with sub value",
+    subValue: "(sub value)",
+  },
+];
+
+const options2 = [
+  { value: "value 1", displayValue: "Display Value" },
+  {
+    value: "value 2",
+    displayValue: "Value with sub value",
+    subValue: "(sub value)",
+  },
+];
 export const Primary: Story = {
   render: () => (
     <Select
       isPrimary={true}
+      options={options}
       placeholder="Placeholder"
       selected={{ value: "SelecteValue", displayValue: "Display Value" }}
     />
@@ -21,5 +74,22 @@ export const Primary: Story = {
 };
 
 export const Secondary: Story = {
-  render: () => <Select isPrimary={false} placeholder="Placeholder" />,
+  render: () => (
+    <Select
+      options={options}
+      isPrimary={false}
+      placeholder="Placeholder"
+      selected={{
+        value: "SelecteValue",
+        displayValue: "Display Value",
+        subValue: "(sub value)",
+      }}
+    />
+  ),
+};
+
+export const LessOptions: Story = {
+  render: () => (
+    <Select options={options2} isPrimary={false} placeholder="Placeholder" />
+  ),
 };
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -6,35 +6,60 @@ import Text from "./Text";
 const Selected = styled.div<{ isPrimary: boolean }>`
   height: 40px;
   display: flex;
+  cursor: default;
   border-radius: 8px;
   flex-direction: row;
   align-items: center;
   box-sizing: border-box;
-  justify-content: left;
+  justify-content: space-between;
   padding: 8px 8px 8px 16px;
 
   background: ${(props) => (props.isPrimary ? "#d8e4fc" : "#FFFFFF")};
   border: 1px solid ${(props) => (props.isPrimary ? "#3c76f1" : "#E1E4E8")};
 `;
 
+const OptionsScroller = styled.div`
+  width: 100%;
+  margin-top: 4px;
+  overflow-x: auto;
+  max-height: 352px;
+  position: absolute;
+  border-radius: 12px;
+  padding: 8px 16px 8px 16px;
+  box-shadow: 0px 8px 24px -6px rgba(0, 0, 0, 0.16),
+    0px 0px 1px rgba(0, 0, 0, 0.4);
+
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #c4c9d1;
+    border-radius: 16px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: #b1b6bd;
+  }
+`;
+
 const Options = styled.ul`
   gap: 4px;
   width: 100%;
   display: flex;
-  margin-top: 4px;
-  padding: 8px 4px;
-  position: absolute;
   background: #ffffff;
-  border-radius: 12px;
   flex-direction: column;
   align-items: flex-start;
-  box-shadow: 0px 8px 24px -6px rgba(0, 0, 0, 0.16),
-    0px 0px 1px rgba(0, 0, 0, 0.4);
 
   li {
     width: 100%;
     padding: 8px;
     display: flex;
+    cursor: pointer;
     border-radius: 8px;
     align-items: flex-start;
   }
@@ -43,8 +68,24 @@ const Options = styled.ul`
   }
 `;
 
+const TextHolder = styled.div`
+  gap: 9px;
+  display: flex;
+  align-items: center;
+
+  //   text
+  span {
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    white-space: pre-wrap;
+    -webkit-box-orient: vertical;
+  }
+`;
+
 interface Ioption {
   value: string | number;
+  subValue?: string | number;
   displayValue: string | number;
 }
 
@@ -54,20 +95,34 @@ const Select = ({
   isPrimary,
   placeholder,
 }: {
-  options?: Ioption[];
   isPrimary: boolean;
   selected?: Ioption;
+  options?: Ioption[];
   placeholder?: string;
 }) => {
   const [selectedOption, setSelected] = useState(selected);
+  const [showOptions, setShowOptions] = useState(false);
+
+  const onOptionSelect = (selectedOption: Ioption) => {
+    setSelected(selectedOption);
+    setShowOptions(false);
+  };
 
   return (
     <div style={{ position: "relative" }}>
-      <Selected isPrimary={isPrimary}>
+      <Selected
+        isPrimary={isPrimary}
+        onClick={() => setShowOptions(!showOptions)}
+      >
         {selectedOption ? (
-          <Text color={isPrimary ? "primary" : "default"} type="button">
-            {selected?.displayValue}
-          </Text>
+          <TextHolder>
+            <Text color={isPrimary ? "primary" : "default"} type="button">
+              {selectedOption?.displayValue}
+            </Text>
+            <Text color="subdued" type="caption">
+              {selectedOption?.subValue}
+            </Text>
+          </TextHolder>
         ) : (
           <Text color="subdued" type="body">
             {placeholder}
@@ -75,10 +130,21 @@ const Select = ({
         )}
       </Selected>
 
-      <Options>
-        <li>item 1</li>
-        <li>item 2</li>
-      </Options>
+      {showOptions && (
+        <OptionsScroller>
+          {options && (
+            <Options>
+              {options.map((option) => (
+                <li key={option.value} onClick={() => onOptionSelect(option)}>
+                  <Text color="default" type="body">
+                    {option?.displayValue}
+                  </Text>
+                </li>
+              ))}
+            </Options>
+          )}
+        </OptionsScroller>
+      )}
     </div>
   );
 };
