863f2f2feef93432a263b07c68894d2ed9d745a0
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2023-06-15 13:49:35 +0530

    fix: Icon comp used in doc list item

--- src/modules/projects/components/DocumentListItem.tsx
+++ src/modules/projects/components/DocumentListItem.tsx
@@ -9,10 +9,9 @@ import { ReactComponent as PptFileIcon } from "../../../assets/icons/fileTypes/p
 import { ReactComponent as PptxFileIcon } from "../../../assets/icons/fileTypes/pptxFileIcon.svg";
 import { ReactComponent as XlsFileIcon } from "../../../assets/icons/fileTypes/xlsFileIcon.svg";
 import { ReactComponent as XlsxFileIcon } from "../../../assets/icons/fileTypes/xlsxFileIcon.svg";
-import { ReactComponent as DownloadIcon } from "../../../assets/icons/generic/download.svg";
 import { ReactComponent as FileIcon } from "../../../assets/icons/generic/file.svg";
-import { ReactComponent as InfoIcon } from "../../../assets/icons/generic/info.svg";
 import Button from "../../../components/Button";
+import Icon from "../../../components/Icon";
 import Text from "../../../components/Text";
 import { convertToDateTimeFormat } from "../../../utils/dateTimeFormatter";
 
@@ -116,7 +115,7 @@ export default function DocumentListItem({
             isIcon={true}
             onClick={() => onGetInfo(documentInfo.id)}
           >
-            <InfoIcon />
+            <Icon name="information" size="xsmall" />
           </Button>
         )}
 
@@ -131,7 +130,7 @@ export default function DocumentListItem({
             )
           }
         >
-          <DownloadIcon />
+          <Icon name="download" size="xsmall" />
         </Button>
       </FlexBox>
     </DocumentItemContainer>
