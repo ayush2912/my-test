1753ab1e3d4bd497a2a8e912f2c9432d5b1f072d
aakashgarg123 <aakashgarg100@gmail.com>
2023-06-19 21:46:39 +0530

    added the doc design

--- src/modules/projects/components/DocumentPreview.stories.tsx
+++ src/modules/projects/components/DocumentPreview.stories.tsx
@@ -9,6 +9,14 @@ const meta: Meta<typeof DocumentPreview> = {
 export default meta;
 type Story = StoryObj<typeof DocumentPreview>;
 
+const documentDetails = {
+  documentName: "TEST DOC 1",
+  fileFormat: "pdf",
+  size: "1.3 MB",
+  source: "Client",
+  date: new Date(),
+};
+
 export const Default: Story = {
-  render: () => <DocumentPreview />,
+  render: () => <DocumentPreview documentDetails={documentDetails} />,
 };
--- src/modules/projects/components/DocumentPreview.tsx
+++ src/modules/projects/components/DocumentPreview.tsx
@@ -1,82 +1,248 @@
-import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
-import { useState, useEffect } from "react";
+import DocViewer, {
+  DocViewerRenderers,
+  IHeaderOverride,
+} from "@cyntler/react-doc-viewer";
+import { useState } from "react";
 import styled from "styled-components";
 
-import TEST from "./Testing Docs.pdf";
+import { ReactComponent as ArrowLeftIcon } from "../../../assets/icons/arrows/arrow-left.svg";
+import { ReactComponent as ArrowRightIcon } from "../../../assets/icons/arrows/arrow-right.svg";
+import { ReactComponent as DocFileIcon } from "../../../assets/icons/fileTypes/docFileIcon.svg";
+import { ReactComponent as DocXFileIcon } from "../../../assets/icons/fileTypes/docXFileIcon.svg";
+import { ReactComponent as JpgFileIcon } from "../../../assets/icons/fileTypes/jpgFileIcon.svg";
+import { ReactComponent as PdfFileIcon } from "../../../assets/icons/fileTypes/pdfFileIcon.svg";
+import { ReactComponent as PngFileIcon } from "../../../assets/icons/fileTypes/pngFileIcon.svg";
+import { ReactComponent as PptFileIcon } from "../../../assets/icons/fileTypes/pptFileIcon.svg";
+import { ReactComponent as PptxFileIcon } from "../../../assets/icons/fileTypes/pptxFileIcon.svg";
+import { ReactComponent as XlsFileIcon } from "../../../assets/icons/fileTypes/xlsFileIcon.svg";
+import { ReactComponent as XlsxFileIcon } from "../../../assets/icons/fileTypes/xlsxFileIcon.svg";
+import { ReactComponent as DownloadIcon } from "../../../assets/icons/generic/download.svg";
+import { ReactComponent as FileIcon } from "../../../assets/icons/generic/file.svg";
+import { ReactComponent as FullScreenIcon } from "../../../assets/icons/generic/full-screen.svg";
+import { ReactComponent as ZoomInIcon } from "../../../assets/icons/generic/zoom-in.svg";
+import { ReactComponent as ZoomOutIcon } from "../../../assets/icons/generic/zoom-out.svg";
+import Button from "../../../components/Button";
+import Text from "../../../components/Text";
+import { convertToDateTimeFormat } from "../../../utils/dateTimeFormatter";
 
 export interface IDocumentDetails {
-  name: string | null;
-  projectId: number | null;
-  engagement: string | null;
   documentName: string | null;
-  state: string | null;
-  fileFormat: string | null;
+  fileFormat: string;
   size: string | null;
   source: string | null;
-  registryApprovalDate: Date | null;
+  date: Date | string;
 }
+
+const HeaderContainer = styled.div`
+  background-color: white;
+  box-shadow: 0px 8px 64px rgba(15, 34, 67, 0.06),
+    0px 0px 1px rgba(15, 34, 67, 0.08);
+  border: 1px solid red;
+  padding: 20px 0px;
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const FlexContainerDocActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 16px;
+`;
+
+const DocInfoContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const DotDivider = styled.div`
+  width: 4px;
+  height: 4px;
+  background-color: #c4c9d1;
+  border-radius: 50%;
+  margin: 0 4px;
+`;
 
 function DocumentPreview({
   documentDetails,
+  handleDownload,
+  handleLeftClick,
+  handleRightClick,
 }: {
   documentDetails: IDocumentDetails;
+  handleDownload: () => void;
+  handleLeftClick: () => void;
+  handleRightClick: () => void;
 }) {
-  // const blobDocs = [
-  //   new Blob([TEST], { type: "application/pdf" }),
-  //   new Blob(["Imagecontent 3"], { type: "image/png" }),
-  // ];
-
-  const url =
-    "https://offsetmax-documents-development.s3.ap-south-1.amazonaws.com/1686569330955.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIASOVJXKXEIX3KPSED%2F20230615%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230615T054446Z&X-Amz-Expires=3600&X-Amz-Signature=55d92065d8456aa3af07e699177827fd34de11e0db22f94d2f9ecbac9bf7efb2&X-Amz-SignedHeaders=host&x-id=GetObject";
-
-  const docsS3 = [
-    {
-      uri: "https://offsetmax-documents-development.s3.ap-south-1.amazonaws.com/1686569330955.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIASOVJXKXEIX3KPSED%2F20230615%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230615T054446Z&X-Amz-Expires=3600&X-Amz-Signature=55d92065d8456aa3af07e699177827fd34de11e0db22f94d2f9ecbac9bf7efb2&X-Amz-SignedHeaders=host&x-id=GetObject",
-    },
-    {
-      uri: "https://offsetmax-documents-development.s3.ap-south-1.amazonaws.com/1686569330955.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIASOVJXKXEIX3KPSED%2F20230615%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230615T054446Z&X-Amz-Expires=3600&X-Amz-Signature=55d92065d8456aa3af07e699177827fd34de11e0db22f94d2f9ecbac9bf7efb2&X-Amz-SignedHeaders=host&x-id=GetObject",
-    },
-    {
-      uri: "https://offsetmax-documents-development.s3.ap-south-1.amazonaws.com/1686569330955.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIASOVJXKXEIX3KPSED%2F20230615%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230615T054446Z&X-Amz-Expires=3600&X-Amz-Signature=55d92065d8456aa3af07e699177827fd34de11e0db22f94d2f9ecbac9bf7efb2&X-Amz-SignedHeaders=host&x-id=GetObject",
-    },
-  ];
+  const fileFormatIcon = {
+    png: <PngFileIcon />,
+    doc: <DocFileIcon />,
+    docx: <DocXFileIcon />,
+    jpg: <JpgFileIcon />,
+    jpeg: <JpgFileIcon />,
+    ppt: <PptFileIcon />,
+    pptx: <PptxFileIcon />,
+    xls: <XlsFileIcon />,
+    xlsx: <XlsxFileIcon />,
+    pdf: <PdfFileIcon />,
+  }[documentDetails.fileFormat];
 
   const docsNormal = [
     {
-      uri: "https://offsetmax-documents-service-dev-japrkvsz2a-em.a.run.app/documents/6486fc8809325e73613ac943/download",
+      uri: "https://upload.wikimedia.org/wikipedia/commons/1/1f/Analytics_Quarterly_Review_Q2_2013_%28Research_and_Data%29.pdf",
+    },
+    {
+      uri: "https://upload.wikimedia.org/wikipedia/commons/3/36/Battling_Browser_Bugs_for_Fun_and_Non-Profit_%28LCA_2015%29.pdf",
     },
   ];
 
-  const [blobs, setBlobs] = useState([]);
+  const [zoom, setZoom] = useState(1);
 
-  useEffect(() => {
-    const fetchBlobs = async () => {
-      for (const doc of docsNormal) {
-        const res = await fetch(doc.uri);
-        // console.log("=========== ", res);
-        const blobb = await res.blob();
-        console.log("BLOBBB     ", window.URL.createObjectURL(blobb));
-        setBlobs((oldArray) => [...oldArray, blobb]);
-      }
-    };
-    fetchBlobs();
-  }, []);
+  const onClickZoomIn = () => {
+    setZoom(zoom + 0.1);
+  };
 
-  console.log("============   ", blobs);
+  const onClickZoomOut = () => {
+    if (zoom > 1) setZoom(zoom - 0.1);
+  };
+
+  const onClickFit = () => {
+    setZoom(1);
+  };
+
+  const MyHeader: IHeaderOverride = (state, previousDocument, nextDocument) => {
+    if (!state.currentDocument || state.config?.header?.disableFileName) {
+      return null;
+    }
+
+    return (
+      <>
+        <HeaderContainer>
+          <FlexContainer>
+            <DocInfoContainer>
+              <div>{fileFormatIcon || <FileIcon />}</div>
+              <div>
+                <Text type="bodyBold" color="default">
+                  {documentDetails.documentName}
+                </Text>
+                <FlexContainer>
+                  <Text type="caption" color="subdued">
+                    {documentDetails.size}
+                  </Text>
+                  <DotDivider />
+                  <Text type="caption" color="subdued">
+                    {convertToDateTimeFormat(documentDetails.date)}
+                  </Text>
+                  <DotDivider />
+                  <Text type="caption" color="subdued">
+                    {documentDetails.source}
+                  </Text>
+                </FlexContainer>
+              </div>
+              {/* <div>{state.currentDocument.uri || ""}</div>
+          <div>
+            <button
+              onClick={previousDocument}
+              disabled={state.currentFileNo === 0}
+            >
+              Previous Document
+            </button>
+            <button
+              onClick={nextDocument}
+              disabled={state.currentFileNo >= state.documents.length - 1}
+            >
+              Next Document
+            </button>
+          </div> */}
+            </DocInfoContainer>
+
+            <FlexContainerDocActions>
+              <Button
+                type="secondary"
+                isIconButton={true}
+                lightBorderColor
+                size="large"
+                onClick={handleDownload}
+              >
+                <DownloadIcon />
+              </Button>
+              <Button
+                type="secondary"
+                isIconButton={true}
+                lightBorderColor
+                size="large"
+                onClick={onClickZoomOut}
+              >
+                <ZoomOutIcon />
+              </Button>
+              <Button
+                type="secondary"
+                isIconButton={true}
+                lightBorderColor
+                size="large"
+                onClick={onClickZoomIn}
+              >
+                <ZoomInIcon />
+              </Button>
+              <Button
+                type="secondary"
+                isIconButton={true}
+                lightBorderColor
+                size="large"
+                onClick={onClickFit}
+              >
+                <FullScreenIcon />
+              </Button>
+              <Text type="body" color="subdued">
+                {`Document 1/50`}
+              </Text>
+              <Button
+                type="secondary"
+                isIconButton={true}
+                lightBorderColor
+                size="large"
+                onClick={handleLeftClick}
+              >
+                <ArrowLeftIcon />
+              </Button>
+              <Button
+                type="secondary"
+                isIconButton={true}
+                lightBorderColor
+                size="small"
+                onClick={handleRightClick}
+              >
+                <ArrowRightIcon />
+              </Button>
+            </FlexContainerDocActions>
+          </FlexContainer>
+        </HeaderContainer>
+      </>
+    );
+  };
 
   return (
     <>
-      {/* {blobs.length && ( */}
       <DocViewer
-        documents={blobs.map((file) => ({
-          uri: window.URL.createObjectURL(file),
-          fileName: file.name,
-        }))}
+        config={{
+          header: {
+            overrideComponent: MyHeader,
+          },
+          pdfVerticalScrollByDefault: true,
+          pdfZoom: {
+            defaultZoom: zoom,
+            // zoomJump:  zoom },
+          },
+        }}
+        documents={docsNormal}
         prefetchMethod="GET"
         pluginRenderers={DocViewerRenderers}
-        // initialActiveDocument={docs[0]}
       />
-      {/* )} */};<h1>dsada</h1>
+      <h1>dsada</h1>
     </>
   );
 }
