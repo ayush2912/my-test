c55f1ad8103e61a47fb4417cf0c0311da3ed3467
Kuan Chien <kcyeo29@gmail.com>
2023-06-23 12:03:19 +0800

    feat: create new tooltip using popper

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,80 +1,67 @@
-import {
-  FunctionComponent,
-  ReactNode,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import { createPortal } from "react-dom";
+import { FunctionComponent, ReactNode, useState } from "react";
+import { usePopper } from "react-popper";
 import styled from "styled-components";
+
+import Text from "./Text";
 
 interface TooltipProps {
   text: string;
   children: ReactNode;
 }
 
-const TooltipContainer = styled.div`
-  position: relative;
-  display: flex;
-`;
-
-const TooltipSpan = styled.span`
+const TooltipContent = styled.div`
   background-color: ${(props) => props.theme.colors.background.card};
   box-shadow: ${(props) => props.theme.shadow.sm};
+
   text-align: center;
   white-space: nowrap;
+
   border-radius: 8px;
   padding: 4px 8px;
   height: 24px;
-
-  position: absolute;
-  z-index: 100;
-
-  bottom: 125%;
-  left: 50%;
-  transform: translateX(-50%);
-
-  font-size: 12px;
-  line-height: 1.2;
-  font-weight: normal;
 `;
 
 const Tooltip: FunctionComponent<TooltipProps> = ({ text, children }) => {
-  const [visible, setVisible] = useState(false);
-  const triggerRef = useRef<HTMLDivElement>(null);
-  const tooltipRef = useRef<HTMLSpanElement>(null);
-  const portalElement = document.getElementById("tooltip-portal");
-
-  useEffect(() => {
-    if (visible && triggerRef.current && tooltipRef.current) {
-      const triggerRect = triggerRef.current.getBoundingClientRect();
-      tooltipRef.current.style.left = `${triggerRect.right}px`;
-      tooltipRef.current.style.top = `${
-        triggerRect.top +
-        window.scrollY -
-        triggerRef.current.offsetHeight / 2 -
-        tooltipRef.current.offsetHeight / 2 -
-        4
-      }px`;
-    }
-  }, [visible]);
+  const [referenceElement, setReferenceElement] =
+    useState<HTMLDivElement | null>(null);
+  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
+    null,
+  );
+  const [showTooltip, setShowTooltip] = useState(false);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: "top",
+    modifiers: [
+      {
+        name: "offset",
+        options: {
+          offset: [0, 4],
+        },
+      },
+    ],
+  });
 
   return (
-    <TooltipContainer
-      ref={triggerRef}
-      onMouseEnter={() => setVisible(true)}
-      onMouseLeave={() => setVisible(false)}
-    >
-      {children}
-      {visible && portalElement
-        ? createPortal(
-            <TooltipSpan ref={tooltipRef} style={{ position: "absolute" }}>
-              {text}
-            </TooltipSpan>,
-            portalElement,
-          )
-        : null}
-    </TooltipContainer>
+    <>
+      <div
+        ref={setReferenceElement}
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => {
+          setShowTooltip(false);
+        }}
+      >
+        {children}
+      </div>
+
+      {showTooltip && (
+        <TooltipContent
+          ref={setPopperElement}
+          style={styles.popper}
+          {...attributes.popper}
+        >
+          <Text type="smallTextBold">{text}</Text>
+        </TooltipContent>
+      )}
+    </>
   );
 };
 
